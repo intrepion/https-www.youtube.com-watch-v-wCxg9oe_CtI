481e372a7adafaf260a7a8bb560ce56c88f2aa1e
Oliver Forral <oliver.forral@insightglobal.com>
2024-08-16 10:38:03 -0600

    dotnet new nunit --name BlazorSsrFileUpload.UnitTests

---
+++ BlazorSsrFileUpload.UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace BlazorSsrFileUpload.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
